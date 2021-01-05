34bd42de2eeef026031a5192dcacb194d8726102
Bruno Donadei <domanbruno@gmail.com>
2021-01-05 15:29:37 -0300

    finalizado

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using MVC_Console.Models;
+using MVC_Console.View;
+
+namespace MVC_Console.Controllers
+{
+    public class ProdutoController
+    {
+      Produto produto = new Produto();  
+      ProdutoView produtoView = new ProdutoView();
+
+      public void ListarProdutos()
+      {
+          produtoView.Listar(produto.Ler());
+      }
+      
+    }
+}
